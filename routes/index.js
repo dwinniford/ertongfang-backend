8476abc16f2b5e4efeb457b0cbcd720b3f62ef71
Dave Winniford <dwinniford@gmail.com>
2020-07-29 07:16:26 -0500

    clean up folders from generator

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'Home Page' });
 });
+
+router.get('/test', function(req, res, next) {
+  
+  res.json({message: "hi there"})
+})
 
 module.exports = router;
