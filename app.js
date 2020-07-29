b41d3820b8ff0566617e15651990175b09e3bc87
Dave Winniford <dwinniford@gmail.com>
2020-07-29 07:43:50 -0500

    sets up cors

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
+const cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+  }
 
 var app = express();
-
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
