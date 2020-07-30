7a0b640c9bb4afa955a7858e2247a944cd7100c9
Dave Winniford <dwinniford@gmail.com>
2020-07-30 13:37:12 -0500

    connects to MongoDB and adds model

---
+++ models/Flashcard.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+
+const flashcardSchema = new mongoose.Schema({
+    front: {
+        type: String,
+        trim: true,
+        required: 'Please enter a word for the front of the card!'
+    },
+    back: {
+        type: String,
+        trim: true,
+        required: 'Please enter a word for the back of the card!'
+    }
+})
+
+module.exports = mongoose.model('Flashcard', storeSchema)
